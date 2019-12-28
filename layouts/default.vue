d65dc7d229a69dced0107fcfb354cbb49047eac3
itcast-hsian <541510140@qq.com>
2019-12-28 16:04:04 +0800

    新建公共组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,49 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+	<div>
+    <Header/>
+		<!-- 相当于router-view,组件的占位符 -->
+		<nuxt />
+    <Footer/>
+	</div>
 </template>
 
+<script>
+// 头部组件
+import Header from "@/components/header";
+// 页面组件
+import Footer from "@/components/footer";
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+* {
+	margin: 0;
+	padding: 0;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+ul,
+li,
+ol {
+	list-style: none;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+a {
+	color: inherit;
+	text-decoration: none;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a:hover {
+	color: inherit;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+i,
+em {
+	font-style: normal;
 }
 </style>
