c56934992259652cd9b90644c6f25dccd7aabb5b
itcast-hsian <541510140@qq.com>
2019-12-29 09:23:32 +0800

    自定义验证用户名

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -1,77 +1,78 @@
 <template>
-    <!-- model: 表单数据对象(指向data中某个属性) -->
-    <!-- ref: 获取dom元素  -->
-    <!-- rules: 验证表单的规则 -->
-    <el-form 
-        :model="form" 
-        ref="form"
-        :rules="rules" 
-        class="form">
+	<!-- model: 表单数据对象(指向data中某个属性) -->
+	<!-- ref: 获取dom元素  -->
+	<!-- rules: 验证表单的规则 -->
+	<el-form :model="form" ref="form" :rules="rules" class="form">
+		<el-form-item class="form-item" prop="username">
+			<el-input placeholder="用户名/手机" v-model="form.username"></el-input>
+		</el-form-item>
 
-        <el-form-item class="form-item">
-            <el-input 
-            placeholder="用户名/手机">
-            </el-input>
-        </el-form-item>
+		<el-form-item class="form-item">
+			<el-input placeholder="密码" type="password" v-model="form.password"></el-input>
+		</el-form-item>
 
-        <el-form-item class="form-item">
-            <el-input 
-            placeholder="密码" 
-            type="password">
-            </el-input>
-        </el-form-item>
+		<p class="form-text">
+			<nuxt-link to="#">忘记密码</nuxt-link>
+		</p>
 
-        <p class="form-text">
-            <nuxt-link to="#">忘记密码</nuxt-link>
-        </p>
-
-        <el-button 
-        class="submit"
-        type="primary"
-        @click="handleLoginSubmit">
-            登录
-        </el-button>
-    </el-form>
-
+		<el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button>
+	</el-form>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            // 表单数据
-            form: {},
-            // 表单规则
-            rules: {},
-        }
-    },
-    methods: {
-        // 提交登录
-        handleLoginSubmit(){
-           console.log(this.form)
-        }
-    }
-}
+	data() {
+        // rule: 定义校验规则（不是必须的）
+        // value: 表单输入框的值
+        // callback: 回调函数，可以接受错误的提示，如果验证通过callback就不用传递参数，callback必须要调用
+		const validateUsername = (rule, value, callback) => {
+            const reg = /^1[3-9][0-9]{9}$/;
+            // 正则下面的test方法返回布尔值
+            if(reg.test(value)){
+                // 验证通过
+                callback();
+            }else{
+                callback("手机号码格式错误");
+            }
+		};
+
+		return {
+			// 表单数据
+			form: {
+				username: "",
+				password: ""
+			},
+			// 表单规则
+			rules: {
+				username: [{ validator: validateUsername, trigger: "blur" }]
+			}
+		};
+	},
+	methods: {
+		// 提交登录
+		handleLoginSubmit() {}
+	}
+};
 </script>
 
 <style scoped lang="less">
-    .form{
-        padding:25px;
-    }
+.form {
+	padding: 25px;
+}
 
-    .form-item{
-        margin-bottom:20px;
-    }
+.form-item {
+	margin-bottom: 20px;
+}
 
-    .form-text{
-        font-size:12px;
-        color:#409EFF;
-        text-align: right;
-        line-height: 1;
-    }
+.form-text {
+	font-size: 12px;
+	color: #409eff;
+	text-align: right;
+	line-height: 1;
+}
 
-    .submit{
-        width:100%;
-        margin-top:10px;
-    }
+.submit {
+	width: 100%;
+	margin-top: 10px;
+}
 </style>
