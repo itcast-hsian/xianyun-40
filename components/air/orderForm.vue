80055af3063d8dd8d4bd4a142c2fad3bc08c684d
itcast-hsian <541510140@qq.com>
2020-01-03 14:51:02 +0800

    乘机人表单的布局

---
+++ components/air/orderForm.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="main">
+        <div class="air-column">
+            <h2>乘机人</h2>
+            <el-form class="member-info">
+                <div class="member-info-item" >
+
+                    <el-form-item label="乘机人类型">
+                        <el-input placeholder="姓名" class="input-with-select">
+                            <el-select 
+                            slot="prepend" 
+                            value="1" 
+                            placeholder="请选择">
+                                <el-option label="成人" value="1"></el-option>
+                            </el-select>
+                        </el-input>
+                    </el-form-item>
+
+                    <el-form-item label="证件类型">
+                        <el-input 
+                        placeholder="证件号码"  class="input-with-select">
+                            <el-select 
+                            slot="prepend" 
+                            value="1"           
+                            placeholder="请选择">
+                                <el-option label="身份证" value="1" :checked="true"></el-option>
+                            </el-select>
+                        </el-input>
+                    </el-form-item>
+
+                    <span class="delete-user" @click="handleDeleteUser()">-</span>
+                </div>
+            </el-form>
+
+            <el-button class="add-member" type='primary' @click="handleAddUsers">添加乘机人</el-button>
+        </div>
+
+        <div class="air-column">
+            <h2>保险</h2>
+            <div>
+                <!-- 循环渲染保险的数据 -->
+                <div class="insurance-item">
+                    <el-checkbox 
+                    label="航空意外险：￥30/份×1  最高赔付260万" 
+                    border>
+                    </el-checkbox> 
+                </div>
+            </div>
+        </div>
+
+        <div class="air-column">
+            <h2>联系人</h2>
+            <div class="contact">
+                <el-form label-width="60px">
+                    <el-form-item label="姓名">
+                        <el-input></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="手机">
+                        <el-input placeholder="请输入内容">
+                            <template slot="append">
+                            <el-button @click="handleSendCaptcha">发送验证码</el-button>
+                            </template>
+                        </el-input>
+                    </el-form-item>
+
+                    <el-form-item label="验证码">
+                        <el-input></el-input>
+                    </el-form-item>
+                </el-form>   
+                <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            users: [],
+            insurances: [], // 保险id
+            contactName: "",
+            contactPhone: "",
+            captcha: "",
+            invoice: false,
+            seat_xid: "", // 座位的id
+            air: ""       // 航班的id
+        }
+    },
+
+    methods: {
+        // 添加乘机人
+        handleAddUsers(){
+            
+        },
+        
+        // 移除乘机人
+        handleDeleteUser(){
+
+        },
+        
+        // 发送手机验证码
+        handleSendCaptcha(){
+            
+        },
+
+        // 提交订单
+        handleSubmit(){
+            
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .air-column{
+        border-bottom:1px #ddd dashed;
+        padding-bottom: 20px;   
+        margin-bottom: 20px;
+    }
+
+    .air-column h2{
+        margin-bottom: 20px;
+        font-size: 22px;
+        font-weight: normal;
+    }
+
+    /deep/ .el-select .el-input {
+        width: 130px;
+    }
+
+    .input-with-select{
+        width:590px;
+    }
+
+    .input-with-select /deep/  .el-input-group__prepend {
+        background-color: #fff;
+    }
+    .member-info /deep/ .el-form-item{
+        margin-bottom:0;
+    }
+
+    .member-info-item{
+        border-bottom:1px #eee dashed;
+        padding-bottom: 20px;
+        position: relative;
+
+        &:first-child{
+            .delete-user{
+                display: none;
+            }
+        }
+    }
+
+    .add-member{
+        margin-top:20px;
+    }
+
+    .delete-user{
+        display: block;
+        background:#ddd;
+        width:16px;
+        height:16px;
+        font-size:14px;
+        text-align: center;
+        line-height: 16px;
+        color:#fff;
+        cursor: pointer;
+        border-radius: 50px;
+        position:absolute;
+        right:-30px;
+        top:50%;
+    }
+
+    .insurance{
+        > div{
+            margin-top:10px;
+        }
+    }
+
+    .insurance-item{
+        margin-bottom: 20px;
+    }
+
+    .contact{
+        /deep/ .el-input{
+            width:50%;
+        }
+    }
+
+    .submit{
+        margin: 50px auto;
+        display: block;
+        width:250px;
+        height:50px;
+    }
+</style>
