45b3a1683258a45e974df123efc4846b83d853d8
itcast-hsian <541510140@qq.com>
2020-01-05 10:00:31 +0800

    登录过期的处理

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="main">
             <div class="pay-title">
-                支付总金额 <span class="pay-price">￥ 1000</span>
+                支付总金额 <span class="pay-price">￥ {{orderData.price}}</span>
             </div>
             <div class="pay-main">
                 <h4>微信支付</h4>
@@ -11,8 +11,10 @@
                 align="middle"
                 class="pay-qrcode">
                     <div class="qrcode">
+
                         <!-- 二维码 -->
                         <canvas id="qrcode-stage"></canvas>
+
                         <p>请使用微信扫一扫</p>
                         <p>扫描二维码支付</p>
                     </div>
@@ -26,8 +28,35 @@
 </template>
 
 <script>
+// 生成二维码的插件
+import QRCode from "QRCode";
+
 export default {
-    
+    data(){
+        return {
+            // 订单详情
+            orderData: {}
+        }
+    },
+    mounted(){
+        // 获取订单id
+        const {id} = this.$route.query;
+        // 请求订单的详情
+        this.$axios({
+            url: "/airorders/" + id,
+            headers: {
+                // Bearer是token字符串前面必须要声明的，后面加上空格，再连接上token
+                Authorization: "Bearer " + this.$store.state.user.userInfo.token
+            }
+        }).then(res => {
+            // 订单的详情
+            this.orderData = res.data;
+            // 获取canvas元素
+            const stage = document.getElementById("qrcode-stage");
+            // 生成二维码
+            QRCode.toCanvas(stage, this.orderData.payInfo.code_url)
+        })
+    }
 }
 </script>
 
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,22 @@
 
 import {Message} from "element-ui";
 
-export default ({$axios}) => {
+export default (nuxt) => {
     // 拦截axios的错误
-    $axios.onError(res => {
+    nuxt.$axios.onError(res => {
         // response是js原生Error对象的属性
         const {statusCode, message} = res.response.data;
 
         if(statusCode === 400){
             // Message相当于this.$message
             Message.error(message)
+        }
+
+        if(statusCode === 401){
+            // Message相当于this.$message
+            Message.error("登录过期，跳转到登录页");
+            // 跳转到登录页
+            nuxt.redirect(200, "/user/login");
         }
     })
 }
