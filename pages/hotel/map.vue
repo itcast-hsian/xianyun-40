f27fc07c1d8a424d0f0d7c9cf18b298cfbf53e43
itcast-hsian <541510140@qq.com>
2020-01-06 11:29:15 +0800

    吉山去北京

--- pages/hotel/map.vue
+++ pages/hotel/map.vue
@@ -23,13 +23,19 @@
                 </el-form-item>
             
                 <el-form-item>
-                    <el-input placeholder="起点位置"></el-input>
+                    <el-input placeholder="起点位置" v-model="start"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-input placeholder="终点位置"></el-input>
+                    <el-input placeholder="终点位置" v-model="end"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button>搜索</el-button>
+                    <el-button @click="handleSearch">搜索</el-button>
+                </el-form-item>
+                <el-form-item v-if="routes[0]">
+                    <div>距离：{{ Number(routes[0].distance / 1000).toFixed(1) }}公里</div>
+                    <div>时间：{{ routes[0].time }}</div>
+                    <div>高速费：{{ routes[0].tolls }}元</div>
+                    <div>高速的距离：{{ Number(routes[0].tolls_distance / 1000).toFixed(1) }}公里</div>
                 </el-form-item>
             </el-form>
             
@@ -42,12 +48,55 @@
 export default {
     data(){
         return {
-            current: 0
+            current: 0,
+
+            // 起点的关键字
+            start: "",
+            // 终点的关键字
+            end: "",
+            // 驾车对象
+            driving: "",
+            // 当前搜索的路线的信息
+            routes: []
         }
     },
     methods: {
+        // tab栏切换
         handleChangeTab(index){
             this.current = index;
+        },
+
+        // 根据搜索关键字路线的规划
+        handleSearch(){
+            var points = [
+                { keyword: this.start, city:'全国' },
+                { keyword: this.end, city:'全国' }
+            ]
+            
+            this.driving.search(points, (status, result) => {
+                if(status === "complete"){
+                    this.routes = result.routes;
+
+                    // 得到分钟
+                    const time = this.routes[0].time / 60;
+                    let hours, min, day;
+                    if(time >= 60){
+                        hours = Math.floor(time / 60);
+                        min = time % 60;
+                        this.routes[0].time = `${hours}小时${Number(min).toFixed(2)}分钟`
+                    }
+                    
+                    if(hours >= 24){
+                        day = Math.floor(hours / 24);
+                        hours = hours % 24;
+                        this.routes[0].time = `${day}天${hours}小时${Number(min).toFixed(2)}分钟`
+                    }
+
+                    if(time < 60){
+                        this.routes[0].time = `${Number(time).toFixed(2)}分钟`
+                    }
+                }
+            })
         }
     },
     mounted(){
@@ -72,19 +121,29 @@ export default {
         // }); 
 
         // 路线规划
-        var driving = new AMap.Driving({
+        // var driving = new AMap.Driving({
+        //     map,
+        //     // 驾车路线规划策略，AMap.DrivingPolicy.LEAST_TIME是最快捷模式
+        //     policy: AMap.DrivingPolicy.LEAST_TIME
+        // })
+        
+        // var startLngLat = [113.4278180000,23.1293300000]
+        // var endLngLat = [113.3245904, 23.1066805]
+        
+        // driving.search(startLngLat, endLngLat, function (status, result) {
+        //     // 未出错时，result即是对应的路线规划方案
+        //     console.log(status, result)
+        // })
+
+
+        // 路线搜索
+        this.driving = new AMap.Driving({
             map,
             // 驾车路线规划策略，AMap.DrivingPolicy.LEAST_TIME是最快捷模式
             policy: AMap.DrivingPolicy.LEAST_TIME
         })
         
-        var startLngLat = [113.4278180000,23.1293300000]
-        var endLngLat = [113.3245904, 23.1066805]
         
-        driving.search(startLngLat, endLngLat, function (status, result) {
-            // 未出错时，result即是对应的路线规划方案
-            console.log(status, result)
-        })
         
     }
 }
